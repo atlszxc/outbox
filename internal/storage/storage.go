35c5048430d7e09367ee256353e2c0a6e0779059
atlszxc <kiakovenko.reon@gmail.com>
2024-02-09 14:37:47 +0300

    fix storage interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,13 @@
 package storage
+
+import (
+	"ot/internal/order/dtos"
+	dtos2 "ot/internal/outbox/dtos"
+)
+
+type Storage interface {
+	CreateOrder(dto dtos.CreateOrderDto) int
+	DeleteOutbox(id int)
+	CreateOutbox(data dtos2.CreateOutboxDto) int
+	UpdateStatusOutbox(id int, complete bool) int
+}
